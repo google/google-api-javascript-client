71450c1f20ea56fad82b15d0e1df33f6f8ee6b1c
Brendan O'Brien <obrien@google.com>
2012-06-25 11:14:25 -0700

    Backup for IO 2012 JS Client slides.

---
+++ io-2012-slides/slide_config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Building Web Applications that use Google APIs and the JavaScript Client for Google APIs',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1',
+    favIcon: 'http://bleedinghtml5.appspot.com/images/chrome-logo-tiny2.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Brendan O\'Brien',
+    company: 'Software Engineer, Google+',
+    gplus: 'https://plus.google.com/116812371442532868406',
+    twitter: '@ICodeJS',
+    www: ''
+  }]
+};
+
